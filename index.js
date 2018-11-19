724acfa44d1618613f76fe4183fdf398cd6094c7
oliverpople <oliverpople@gmail.com>
2018-11-19 11:15:19 -0800

    Reduced responsibility of getTranscript function, improved naming and flow.

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ app.get("/keywords", function(req, res) {
   var file = __dirname + "/audio/" + fileId;
   fs.exists(file, async function(exists) {
     if (exists) {
-      var keywords = await getKeywords(file);
-      res.send(keywords);
+      var transcriptText = await getTranscript(file);
+      var extractedKeyWords = extractKeyWords(transcriptText);
+      res.send(extractedKeyWords);
     } else {
       res.send("Its a 404");
       res.end();
@@ -41,29 +42,27 @@ app.get("/keywords", function(req, res) {
   });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-async function getKeywords(file) {
+async function getTranscript(file) {
   try {
     const transcript = new assemblyai.Upload(file);
     const response = await transcript.create();
     const data = response.get();
-    var text = data.text;
-    var extractedKeywords = extractKeyWords(text);
-    // console.log(extractedKeywords);
-    return extractedKeywords;
+    var transcriptText = data.text;
+    return transcriptText;
   } catch (e) {
     // Do some error handling here
   }
 }
 
 function extractKeyWords(text) {
-  var extractedKeywords = keyword_extractor.extract(text, {
+  var extractedKeyWords = keyword_extractor.extract(text, {
     language: "english",
     remove_digits: true,
     return_chained_words: true,
     return_changed_case: true,
     remove_duplicates: false
   });
-  return extractedKeywords;
+  return extractedKeyWords;
 }
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

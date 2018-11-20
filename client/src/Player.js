5106dba803f58e6a27c0e875e3d7f49c942c4266
oliverpople <oliverpople@gmail.com>
2018-11-20 12:50:09 -0800

    The generated list of keywords render as buttons which can be selected. onClick each keyword that a button represents is added to selectKeyWords array in the components state.  Currently the first key word isn’t being added to the array, which needs fixing.

--- client/src/Player.js
+++ client/src/Player.js
@@ -6,27 +6,23 @@ class Player extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "",
-      keywords: ""
+      url: "",
+      keywords: "",
+      selectKeyWords: []
     };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderKeywords = this.renderKeywords.bind(this);
+    this.selectKeyWord = this.selectKeyWord.bind(this);
   }
 
-  // async componentDidMount() {
-  //   const res = await axios.get("/keywords?id=bbcminute.mp3");
-  //   var keywordArray = res.data;
-  //   this.setState({ keywords: keywordArray });
-  // }
-
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ url: event.target.value });
   }
 
   async handleSubmit(event) {
     event.preventDefault();
-    var url = this.state.value;
+    var url = this.state.url;
     const res = await axios.get("/keywords/url", {
       params: {
         link: url
@@ -36,15 +32,25 @@ class Player extends Component {
     this.setState({ keywords: keywordArray });
   }
 
+  // <a href={`https://www.google.ca/search?q=${keyword}`}>        </a>
+  selectKeyWord(event) {
+    this.setState({
+      selectKeyWords: [...this.state.selectKeyWords, event.target.value]
+    });
+    console.log(this.state.selectKeyWords);
+  }
+
   renderKeywords() {
     if (this.state.keywords) {
       var keywords = this.state.keywords.map(function(keyword, i) {
         return (
           <li key={i}>
-            <a href={`https://www.google.ca/search?q=${keyword}`}>{keyword}</a>
+            <button value={keyword} onClick={this.selectKeyWord}>
+              {keyword}
+            </button>
           </li>
         );
-      });
+      }, this);
       return keywords;
     }
   }
@@ -57,19 +63,14 @@ class Player extends Component {
             URL:
             <input
               type="text"
-              value={this.state.value}
+              value={this.state.url}
               name="name"
               onChange={this.handleChange}
             />
           </label>
           <input type="submit" value="Submit" />
         </form>
-        <ReactPlayer
-          url={this.state.value}
-          controls={true}
-          height={56}
-          playing
-        />
+        <ReactPlayer url={this.state.url} controls={true} height={56} playing />
         {this.renderKeywords()}
       </div>
     );
